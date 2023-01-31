0bb983b5d2799ca76fd9cda36fc1dceef64e39ef
DuyLinh <linhvhd@caerux.com>
2023-01-31 23:24:31 +0700

    change logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-
+	<link rel="icon" href="/img/logo.png">
 	<title>{{ config('app.name', 'Laravel') }}</title>
 
 	<!-- Fonts -->
@@ -51,7 +51,7 @@
 		<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
 			<div class="container">
 				<a class="navbar-brand" href="{{ url('/') }}">
-					{{ config('app.name', 'Laravel') }}
+					<img src="/img/logo.png" alt="" height="40" >
 				</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
 					aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
