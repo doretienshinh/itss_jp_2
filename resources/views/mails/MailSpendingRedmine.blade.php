7339c489eff469c38256ebcca34352510bd1bb9c
DuyLinh <linhvhd@caerux.com>
2023-02-02 14:42:47 +0700

    hot fix url in mail

--- resources/views/mails/MailSpendingRedmine.blade.php
+++ resources/views/mails/MailSpendingRedmine.blade.php
@@ -1,5 +1,5 @@
 <h2>Xin chào, {{ $user->name }}</h2> 
 <br>
-<p>Xin hãy điền thu chi hôm nay của bạn vào <a href="https://spendee.binhnguyen.tech/">Spendee</a></p>
+<p>Xin hãy điền thu chi hôm nay của bạn vào <a href="http://spendee.binhnguyen.tech/">Spendee</a></p>
     
 Arigathanks.
