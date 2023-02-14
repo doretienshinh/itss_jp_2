432fd4ccc0612f794d03e818bed6914e73c261dd
binh.nguyenduc2000 <binh.nguyenduc2000@gmail.com>
2023-02-14 18:28:40 +0700

    Update ava shape to circle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 	<!-- Fonts -->
 	<link rel="dns-prefetch" href="//fonts.gstatic.com">
 	<link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 	<!-- Scripts -->
 	@vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -23,9 +23,11 @@
 			margin: 0;
 			padding: 0;
 		}
+
 		input:focus {
-  			outline: none;
+			outline: none;
 		}
+
 		.table td {
 			border-top: 0;
 		}
@@ -36,11 +38,11 @@
 			flex-flow: column;
 		}
 
-		#app > nav {
+		#app>nav {
 			flex: 0 1 auto;
 		}
 
-		#app > main {
+		#app>main {
 			flex: 1 1 auto;
 		}
 
@@ -55,9 +57,9 @@
 		}
 
 		.nav-app.active,
-		.nav-app:hover{
+		.nav-app:hover {
 			border: 1pt solid black;
-			background-color: #F5F5F5 !important; 
+			background-color: #F5F5F5 !important;
 			color: black !important;
 			font-weight: bold !important;
 		}
@@ -74,8 +76,7 @@
 	<div id="app">
 		<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
 			<div class="container">
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-					aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
@@ -85,26 +86,23 @@
 
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav ms-auto">
-					<!-- Left Side Of Navbar -->
+						<!-- Left Side Of Navbar -->
 					</ul>
 
 					<!-- Right Side Of Navbar -->
 					<ul class="navbar-nav me-auto me-md-5">
 						<li class="nav-item nav-app {{ (request()->is('/home') || request()->is('/') || request()->is('home-page') ) ? 'active' : '' }}">
-							<a class="nav-link"
-							aria-current="page" href="{{ route('home-page') }}">
+							<a class="nav-link" aria-current="page" href="{{ route('home-page') }}">
 								Thu chi
 							</a>
 						</li>
 						<li class="nav-item nav-app {{ (request()->is('monthly-income') ) ? 'active' : '' }}">
-							<a class="nav-link"
-							href="{{ route('monthly-income') }}">
+							<a class="nav-link" href="{{ route('monthly-income') }}">
 								Báo cáo
 							</a>
 						</li>
 						<li class="nav-item nav-app {{ (request()->is('wallet-setting') ) ? 'active' : '' }}">
-							<a class="nav-link"
-							href="{{ route('wallet-setting') }}">
+							<a class="nav-link" href="{{ route('wallet-setting') }}">
 								Thiết lập
 							</a>
 						</li>
@@ -131,8 +129,7 @@
 
 							<div class="col-md-4 gradient-custom text-center" style="border-bottom: 1px solid lightgrey">
 								<a href="{{ route('profile') }}">
-									<img src="{{ (auth()->user()->avatar ? "/storage/avatar/" . auth()->user()->avatar : "https://cdn-icons-png.flaticon.com/512/4128/4128176.png") }}" alt="Avatar" class="img-fluid my-4"
-										style="width: 100px; height: 100px" />
+									<img src="{{ (auth()->user()->avatar ? "/storage/avatar/" . auth()->user()->avatar : "https://cdn-icons-png.flaticon.com/512/4128/4128176.png") }}" alt="Avatar" class="img-fluid my-4" style="width: 100px; height: 100px; border-radius: 50%;" />
 									<h5 style="color: black">{{ auth()->user()->name }}</h5>
 								</a>
 							</div>
@@ -158,7 +155,7 @@
 						</div>
 
 						<div class="flex-grow-0 flex-shrink-1" style="height: 68vh; overflow-y: auto; overflow-x: hidden" id="content_body">
-						@yield('content')
+							@yield('content')
 						</div>
 					</div>
 				</div>
@@ -169,4 +166,5 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </html>
